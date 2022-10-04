f253eac00d9d52c009a076c64f397c1bc4d880ed
Teje8 <jteruel8@gmail.com>
2022-10-04 17:19:46 -0300

    agregado de rutas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const canchasRouter = require('./Canchas')
+const comentariosRouter = require('./Comentario')
+const usuarioRouter = require('./Usuario')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sportacus' });
 });
 
+
+router.use('/canchas', canchasRouter)
+router.use('/comentarios', comentariosRouter)
+router.use('/usuarios', usuarioRouter)
 module.exports = router;
