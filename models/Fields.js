ce2ebdce5c3cac3fa8e5ed59fb605af9bd482936
juanrivarola <jeryflr@gmail.com>
2022-10-06 18:00:27 -0300

    fix

--- models/Fields.js
+++ models/Fields.js
@@ -10,7 +10,7 @@ const schema= new mongoose.Schema({
     description:{type: String, required:true}
 })
 
-const Cancha = mongoose.model(
+const Field = mongoose.model(
     'fields',
     schema
 )
