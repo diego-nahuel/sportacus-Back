7f196feddc1c752cef6b36dd3ee13c0cbc48579f
Teje8 <jteruel8@gmail.com>
2022-10-06 17:20:10 -0300

    arreglo idioma

---
+++ models/Fields.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose')
+
+const schema= new mongoose.Schema({
+    name:{type: String, required:true},
+    user:{type: mongoose.Types.ObjectId, ref: 'users', required: true},
+    city:{type: String, required:true},
+    price:{type: Number, required:true},
+    image:{type:String, required:true},
+    likes:{type: Array, required:true},
+    description:{type: String, required:true}
+})
+
+const Cancha = mongoose.model(
+    'fields',
+    schema
+)
+
+module.exports = Field
