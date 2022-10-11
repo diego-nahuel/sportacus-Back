9e79c37bd51260a8cf88251a23e54f8ebd4aa285
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-11 18:20:57 -0300

    added validation for sport field

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -2,130 +2,136 @@ const Product = require('../models/Products')
 const Joi = require('joi')
 
 const validator = Joi.object({
-    "name":Joi.string().min(4).message("El nombre ingresado es muy corto").max(30).message("El nombre ingresado es muy largo"),
-    "image":Joi.string().uri().message("La imagen no corresponde a un Url valido"),
-    "price":Joi.number().min(1).message("El precio debe ser mayor a 0"),
-    "description":Joi.string().min(8).message("La descripcion ingresada es muy corta")
+    "name": Joi.string().min(4).message("El nombre ingresado es muy corto").max(30).message("El nombre ingresado es muy largo"),
+    "sport": Joi.string().min(1).message("El nombre del deporte ingresado es muy corto").max(48).message("El nombre del deporte ingresado es muy largo"),
+    "type": Joi.string().min(0).message("El precio debe ser mayor a 0"),
+    "price": Joi.number().min(1).message("El precio debe ser mayor a 0"),
+    "stock": Joi.number().min(0).message("El stock no puede ser menor a 0"),
+    "image": Joi.string().uri().message("La imagen no corresponde a un Url valido"),
+    "description": Joi.string().min(8).message("La descripcion ingresada es muy corta")
 })
 
-const productController ={
-    create: async(req, res)=>{
-        const{name, image, price,description}=req.body
-        try{
-         await validator.validateAsync(req.body)
-         let product = await new Product(req.body).save()
-         
-         if(product){res.status(201).json({
-            message:"Producto creado con exito",
-            response:product,
-            success:true
-         })}
-        }catch(error){
-            console.log(error)
-            res.status(400).json({
-                message:error.message,
-                success:false
-            })
-        }
-    },
-    update: async (req, res)=>{
-        const {id} = req.params
-        const modifyC = req.body
-        
-        try{
-           await validator.validateAsync(req.body)
-           let product = await Product.findOneAndUpdate({_id:id}, modifyC,{new: true})
-           if (product) {
-            res.status(200).json({
-                message:"Producto editado con exito",
-                response: product,
-                success:true
-            })
-           } else {
-            res.status(404).json({
-                message:"Producto no encontrado",
-                success:false
-            })
-           }
-        }catch (error){
+const productController = {
+    create: async (req, res) => {
+        const { name, image, price, description } = req.body
+        try {
+            await validator.validateAsync(req.body)
+            let product = await new Product(req.body).save()
+
+            if (product) {
+                res.status(201).json({
+                    message: "Producto creado con exito",
+                    response: product,
+                    success: true
+                })
+            }
+        } catch (error) {
             console.log(error)
             res.status(400).json({
                 message: error.message,
                 success: false
             })
         }
-    },destroy: async(req, res)=>{
-        const {id} = req.params
-        try{
-            let product = await Product.findOneAndDelete({_id:id})
+    },
+    update: async (req, res) => {
+        const { id } = req.params
+        const modifyC = req.body
+
+        try {
+            await validator.validateAsync(req.body)
+            let product = await Product.findOneAndUpdate({ _id: id }, modifyC, { new: true })
             if (product) {
                 res.status(200).json({
-                    message:"Producto eliminado con exito",
-                    success:true
+                    message: "Producto editado con exito",
+                    response: product,
+                    success: true
                 })
-               } else {
+            } else {
                 res.status(404).json({
-                    message:"Producto no encontrado",
-                    success:false
-                })
-               }
-        }catch (error){
-             console.log(error)
-             res.status(400).json({
-                message:"Error, profucto no eliminado",
-                success:false
-             })
-        }
-    },
-    all: async(req,res)=>{
-        let products
-        let query={}
-        if (req.query.name) {
-            let regExp = new RegExp(`^${req.query.name}`,"i")
-            query.name = regExp
-        }
-        try{
-            products = await Product.find(query)
-           if(products){ res.status(200).json({
-                message:"Estos son todos los productos",
-                response:products,
-                success:true
-            })
-        }else{
-            res.status(404).json({
-                message:'No se encontraron productos',
-                success:false
-            })
-        }
-        }catch(error){
-            console.log(error)
-            res.status(400).json({
-                message:'Error, productos no encontrados',
-                success:false
-            })
-        }
-    },
-    readOne: async(req, res)=>{
-        const {id}=req.params
-        try{
-            let product = await Product.findOne({_id:id})
-            if(product){
-                res.status(200).json({
-                    message:"Este es el producto que buscabas",
-                    response:product,
-                    success:true
-                })
-            }else{
-                res.status(404).json({
-                    message:'No se encontro el producto',
-                    success:false
+                    message: "Producto no encontrado",
+                    success: false
                 })
             }
-        }catch(error){
+        } catch (error) {
             console.log(error)
             res.status(400).json({
-                message:"Error, producto no encontrado",
-                success:false
+                message: error.message,
+                success: false
+            })
+        }
+    }, destroy: async (req, res) => {
+        const { id } = req.params
+        try {
+            let product = await Product.findOneAndDelete({ _id: id })
+            if (product) {
+                res.status(200).json({
+                    message: "Producto eliminado con exito",
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: "Producto no encontrado",
+                    success: false
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({
+                message: "Error, profucto no eliminado",
+                success: false
+            })
+        }
+    },
+    all: async (req, res) => {
+        let products
+        let query = {}
+        if (req.query.name) {
+            let regExp = new RegExp(`^${req.query.name}`, "i")
+            query.name = regExp
+        }
+        try {
+            products = await Product.find(query)
+            if (products) {
+                res.status(200).json({
+                    message: "Estos son todos los productos",
+                    response: products,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: 'No se encontraron productos',
+                    success: false
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({
+                message: 'Error, productos no encontrados',
+                success: false
+            })
+        }
+    },
+    readOne: async (req, res) => {
+        const { id } = req.params
+        try {
+            let product = await Product.findOne({ _id: id })
+            if (product) {
+                res.status(200).json({
+                    message: "Este es el producto que buscabas",
+                    response: product,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: 'No se encontro el producto',
+                    success: false
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({
+                message: "Error, producto no encontrado",
+                success: false
             })
         }
     }
