5d4f52e4b5922c5a1b8e7fa2bc8b879630455007
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-11 15:52:56 -0300

    added properties to Products and Fields models

--- models/Products.js
+++ models/Products.js
@@ -1,10 +1,14 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    name:{type: String, required:true},
-    image:{type: String, required:true},
-    price:{type:Number, required:true},
-    description:{type: String, required:true}
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    stock: { type: String, required: true },
+    sport: [{ type: String, required: true }],
+    type: [{ type: String, required: true }],
+    comments: [{ type: mongoose.Types.ObjectId, ref: 'comments' }]
 })
 
 const Product = mongoose.model(
