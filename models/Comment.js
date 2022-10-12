32f4552141fb02a8479314fc98a251b93119f4d0
juanrivarola <jeryflr@gmail.com>
2022-10-12 12:50:00 -0300

    fix

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
     comment:{type: String, required: true},
     user:{type: mongoose.Types.ObjectId, ref: 'users', required: true},
-    field:{type: mongoose.Types.ObjectId, ref: 'canchas'},
+    field:{type: mongoose.Types.ObjectId, ref: 'fields'},
     // product:{type: mongoose.Types.ObjectId, ref: 'products'},
 })
 const Comment = mongoose.model(
