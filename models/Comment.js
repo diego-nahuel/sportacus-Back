0f58d33c894c4d6d61233737cfa9b16c4aa1a0fa
juanrivarola <jeryflr@gmail.com>
2022-10-12 12:20:28 -0300

    saque products

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,7 @@ const schema = new mongoose.Schema({
     comment:{type: String, required: true},
     user:{type: mongoose.Types.ObjectId, ref: 'users', required: true},
     field:{type: mongoose.Types.ObjectId, ref: 'canchas'},
-    product:{type: mongoose.Types.ObjectId, ref: 'products'},
+    // product:{type: mongoose.Types.ObjectId, ref: 'products'},
 })
 const Comment = mongoose.model(
     'comments', 
