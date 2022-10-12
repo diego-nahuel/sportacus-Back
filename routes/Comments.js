fd92d1da717872c95a926be7bd67c21714d1bb32
juanrivarola <jeryflr@gmail.com>
2022-10-12 12:50:14 -0300

    fix

--- routes/Comments.js
+++ routes/Comments.js
@@ -6,10 +6,12 @@ const {CreateComment,
     OneComment,
     UpdateComment,
     DeleteComment,
+    getCommentFromField
 } = require('../controllers/CommentController')
 
 router.post('/', passport.authenticate('jwt', {session: false}), CreateComment)
 router.get('/', AllComments)
+router.get('/query', getCommentFromField)
 router.get('/:id', passport.authenticate('jwt', {session: false}), OneComment)
 router.patch('/:id', passport.authenticate('jwt', {session: false}), UpdateComment)
 router.delete('/:id', passport.authenticate('jwt', {session: false}), DeleteComment)
